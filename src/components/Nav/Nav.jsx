9020242369e14f407fc3b1d2dfd0c8ce9b15ab6f
AnaCarolina86 <anaccm86@gmail.com>
2020-08-22 15:58:38 +0200

    fix bug in the navigation bar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -12,6 +12,7 @@ function Nav(){
 
     function handleLink(){
         document.querySelector("#link").classList.remove([styles.navOpen]);
+        document.querySelector("#menu").classList.toggle([styles.navOpen]);
         setIsOpen(false);
     }
 
@@ -20,7 +21,7 @@ function Nav(){
             <nav className={styles.nav} id="navSection" style={{transform: isOpen? "translateX(0px)" : "translateX(100%)"}} >
                 <ul className={styles.navList}>
                     <li><Link to="/" className={styles.navLink} id= "link" onClick={handleLink} >Página Inicial</Link></li>
-                    <li><Link to="historia" className={styles.navLink} id= "link" onClick={handleLink} >Nossa História</Link></li>
+                    <li><Link to="history" className={styles.navLink} id= "link" onClick={handleLink} >Nossa História</Link></li>
                     <li><Link to="bikini" className={styles.navLink} id= "link" onClick={handleLink} >Moda Praia</Link></li>
                     <li><Link to="personalizado" className={styles.navLink} id= "link" onClick={handleLink} >Personalizado</Link></li>
                     <li><Link to="blog" className={styles.navLink} id= "link" onClick={handleLink} >Blog</Link></li>
