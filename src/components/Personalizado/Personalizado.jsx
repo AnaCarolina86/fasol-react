37c804af0e8b8ee6b4f84db99b8acfdbeb37142b
AnaCarolina86 <anaccm86@gmail.com>
2020-11-11 18:15:12 +0100

    update personalizado page, not finished

--- src/components/Personalizado/Personalizado.jsx
+++ src/components/Personalizado/Personalizado.jsx
@@ -2,33 +2,40 @@ import React from "react";
 import PersonalizadoForm from "../PersonnalizadoForm/PersonalizadoForm";
 import Footer from "../Footer/Footer";
 
-function Personalizado(){
+import styles from "./Personalizado.module.css";
+
+function Personalizado() {
     return (
         <div>
-            <section>
-            <div>
-                <h2>Somos diversos! O nosso corpo e gosto é único!</h2>
-                <h2>É pensando nisso que personalizamos a moda praia do seu jeito, com suas medidas, com o modelo que deseja, cor e estampa. 
+            <section className={styles.personSection}>
+                <div className={styles.titlesPerson}>
+                    <h2 className={styles.diverseTitlePerson}>Somos diversos! O nosso corpo e gosto é único!</h2>
+                    <h2 className={styles.subtitlePerson}>É pensando nisso que personalizamos a moda praia do seu jeito, com suas medidas, com o modelo que deseja, cor e estampa.<br />
                     Preencha seus dados abaixo e faça o seu orçamento:
-                </h2>
-            </div>
+                    </h2>
+                </div>
 
-            <h1>Formulário</h1>
+                <h1 className={styles.titleFormPerson}>Formulário</h1>
 
-            <PersonalizadoForm />
+                <PersonalizadoForm />
 
-            <section>
-                <h3>MULHER - COMO TIRAR SUAS MEDIDAS</h3>
-                <img src="./images/tabela-medidas-feminino1.png" alt="table that shows how to take feminine body mesuares bikini" />
-                <img src="./images/tabela-medidas-feminino2.png" alt="table that shows how to take feminine body mesuares bikini" />
+                <section>
+                    <h2 className={styles.titleHowPerson}>MULHER - COMO TIRAR SUAS MEDIDAS</h2>
+                    <div className={styles.imagesHowPerson}>
+                        <img className={styles.imageHowPerson} src="./images/tabela-medidas-feminino1.png" alt="table that shows how to take feminine body mesuares bikini" />
+                        <img className={styles.imageHowPerson} src="./images/tabela-medidas-feminino2.png" alt="table that shows how to take feminine body mesuares bikini" />
+                    </div>
 
-                <h3>HOMEM - COMO TIRAR SUAS MEDIDAS</h3>
-                <img src="./images/tabela-medidas-masculino.png" alt="able that shows how to take masculine body mesuares roupa de banho" />
-            </section>
+                    <h2 className={styles.titleHowPerson} >HOMEM - COMO TIRAR SUAS MEDIDAS</h2>
+                    <div className={styles.imagesHowPerson}>
+                        <img className={styles.imageHowPerson} src="./images/tabela-medidas-masculino.png" alt="able that shows how to take masculine body mesuares roupa de banho" />
+                    </div>
+
+                </section>
             </section>
             <Footer />
         </div>
-        
+
     );
 }
 
